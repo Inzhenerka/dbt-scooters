9bd3aadacbac6404205e6e79ef3879e6fd8c3d63
Pavel Roslovets <pashokros@yandex.ru>
2024-04-18 02:28:21 +0000

    feat: incremental trips_concurrency

---
+++ models/trips_concurrency.sql
@@ -0,0 +1,52 @@
+with
+unnest_cte as (
+    select
+        unnest(array[started_at, finished_at]) as "timestamp",
+        unnest(array[1, -1]) as increment
+    from
+        {{ source("scooters_raw", "trips") }}
+),
+sum_cte as (
+    select
+        "timestamp",
+        sum(increment) as increment,
+        true as output_data
+    from
+        unnest_cte
+    where
+    {% if is_incremental() %}
+        "timestamp" > (select max("timestamp") from {{ this }})
+    {% else %}
+        "timestamp" < (date '2023-06-01' + interval '7' hour) at time zone 'Europe/Moscow'
+    {% endif %}
+    group by
+        1
+    {% if is_incremental() %}
+    union all
+    select
+        "timestamp",
+        concurrency as increment,
+        false as output_data
+    from
+        {{ this }}
+    where
+        "timestamp" = (select max("timestamp") from {{ this }})
+    {% endif %}
+),
+cumsum_cte as (
+    select
+        "timestamp",
+        sum(increment) over (order by "timestamp") as concurrency,
+        output_data
+    from
+        sum_cte
+)
+select
+    "timestamp",
+    concurrency
+from
+    cumsum_cte
+where
+    output_data
+order by
+    1
