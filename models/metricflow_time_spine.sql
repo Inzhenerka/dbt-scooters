706313e1cfd651a002f53790c047457d64672412
Pavel Roslovets <p.v.roslovets@gmail.com>
2024-06-18 02:42:27 +0200

    feat: semantic layer for trips_users

---
+++ models/metricflow_time_spine.sql
@@ -0,0 +1,13 @@
+with days as (
+    {{ dbt.date_spine(
+        'day',
+        "date '2023-06-01'",
+        "date '2023-08-31'"
+    ) }}
+),
+final as (
+    select cast(date_day as date) as date_day
+    from days
+)
+select *
+from final
