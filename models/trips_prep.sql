ec31e2af0fb89763f7077b1f065734cc22e061f3
Pavel Roslovets <p.v.roslovets@gmail.com>
2024-11-02 01:33:09 +0100

    feat: prepare trips data, introduce modeling

---
+++ models/trips_prep.sql
@@ -0,0 +1,17 @@
+select
+    id,
+    user_id,
+    scooter_hw_id,
+    started_at,
+    finished_at,
+    start_lat,
+    start_lon,
+    finish_lat,
+    finish_lon,
+    distance as distance_m,
+    cast(price as decimal(20, 2)) / 100 as price_rub,
+    extract(epoch from (finished_at - started_at)) as duration_s,
+    finished_at <> started_at and price = 0 as is_free,
+    date(started_at) as "date"
+from
+    {{ source("scooters_raw", "trips") }}
--- models/trips_stat_daily.sql
+++ models/trips_stat_daily.sql
@@ -1,10 +1,11 @@
 select
-    date(started_at) as date,
+    "date",
     count(*) as trips,
-    max(price) / 100 as max_price_rub,
-    avg(distance) / 1000 as avg_distance_km
+    max(price_rub) as max_price_rub,
+    avg(distance_m) / 1000 as avg_distance_km,
+    avg(price_rub) / avg(duration_s) * 60 as avg_price_rub_per_min
 from
-    scooters_raw.trips
+    {{ ref("trips_prep") }}
 group by
     1
 order by
