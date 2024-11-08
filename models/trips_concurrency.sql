06a3daa9d42c91c52bbe0de220408299f3083e04
Pavel Roslovets <p.v.roslovets@gmail.com>
2024-11-08 17:26:30 +0100

    feat: incremental trips_concurrency
    with window function

---
+++ models/trips_concurrency.sql
@@ -0,0 +1,55 @@
+with
+unnest_cte as (
+    -- Unnest trip to two rows: start and finish events
+    select
+        unnest(array[started_at, finished_at]) as "timestamp",
+        unnest(array[1, -1]) as increment
+    from
+        {{ source("scooters_raw", "trips") }}
+),
+sum_cte as (
+    -- Make timestamp unique, group increments, add initial concurrency from history
+    select
+        "timestamp",
+        sum(increment) as increment,
+        true as preserve_row
+    from
+        unnest_cte
+    where
+    {% if is_incremental() %}
+        "timestamp" > (select max("timestamp") from {{ this }})
+    {% else %}
+        "timestamp" < (date '2023-06-01' + interval '7' hour) at time zone 'Europe/Moscow'
+    {% endif %}
+    group by
+        1
+    {% if is_incremental() %}
+    union all
+    select
+        "timestamp",
+        concurrency as increment,
+        false as preserve_row
+    from
+        {{ this }}
+    where
+        "timestamp" = (select max("timestamp") from {{ this }})
+    {% endif %}
+),
+cumsum_cte as (
+    -- Integrate increment to get concurrency
+    select
+        "timestamp",
+        sum(increment) over (order by "timestamp") as concurrency,
+        preserve_row
+    from
+        sum_cte
+)
+select
+    "timestamp",
+    concurrency
+from
+    cumsum_cte
+where
+    preserve_row
+order by
+    1
