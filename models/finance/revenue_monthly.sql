f8a7a4af4f32cf90b38ed6795865e768e65ad0c8
Pavel Roslovets <p.v.roslovets@gmail.com>
2024-12-26 16:49:07 +0100

    feat: incremental revenue report

---
+++ models/finance/revenue_monthly.sql
@@ -0,0 +1,11 @@
+select
+    "month",
+    count(*) as users,
+    percentile_cont(0.5) within group (order by revenue_total) as revenue_median,
+    percentile_cont(0.95) within group (order by revenue_total) as revenue_95,
+    max(revenue_total) as revenue_max,
+    sum(revenue_total) as revenue_total
+from
+    {{ ref("revenue_user_monthly") }}
+group by
+    1
