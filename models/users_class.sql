4a3ec60e907fda3652e91a50db10a17b6b98008e
Pavel Roslovets <p.v.roslovets@gmail.com>
2025-01-16 02:32:13 +0100

    style: users_class formatting

--- models/users_class.sql
+++ models/users_class.sql
@@ -1,15 +1,15 @@
 select
-    id as user_id,
-    fan,
-    regular,
-    rare,
-    to_work,
-    not(fan or regular or rare or to_work) as no_class
+    u.id as user_id,
+    w.fan,
+    w.regular,
+    m.rare,
+    wd.to_work,
+    not(w.fan or w.regular or m.rare or wd.to_work) as no_class
 from
-    {{ ref('users_prep') }}
-full outer join {{ ref('users_class_weekly_trips') }}
-    on user_id = id
-full outer join {{ ref('users_class_weekly_destination_trips') }}
+    {{ ref('users_prep') }} as u
+full outer join {{ ref('users_class_weekly_trips') }} as w
+    on w.user_id = u.id
+full outer join {{ ref('users_class_weekly_destination_trips') }} as wd
     using (user_id)
-full outer join {{ ref('users_class_monthly_trips') }}
+full outer join {{ ref('users_class_monthly_trips') }} as m
     using (user_id)
